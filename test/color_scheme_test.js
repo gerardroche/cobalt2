a4fb3f4e68708dccb22985855c5b43c9b199e345
Gerard Roche <gerardroche@users.noreply.github.com>
2016-08-19 16:30:02 +0100

    Add color_scheme_unit tests
    See https://github.com/gerardroche/sublime_color_scheme_unit

---
+++ test/color_scheme_test.js
@@ -0,0 +1,14 @@
+// COLOR SCHEME TEST "Theme - Cobalt2/cobalt2.tmTheme" "JavaScript"
+
+// comment
+// ^ fg=#0088ff fs=italic
+
+jQuery.fn = jQquery.prototype = {
+// ^ fg=#cccccc bg=#0d3a58 fs=
+//    ^ fg=#e1efff fs=
+//     ^^ fg=#ffffff fs=
+//        ^ fg=#ff9d00 fs=
+//              ^ fg=#80ffbb fs=
+//                 ^ fg=#e1efff fs=
+//                      ^ fg=#eb939a fs=
+}
