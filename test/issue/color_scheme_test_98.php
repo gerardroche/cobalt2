78a54f0d672ade7d1340cc8369bd922e80dbac24
Gerard Roche <gerardroche@users.noreply.github.com>
2016-08-19 16:36:37 +0100

    Fix docblock issue where storage modifier words are not correct color
    For example, the second parameter in the following should all be the
    same colour:
    
        /**
         * @param array
         * @param public
         * @access public
         */
    
    Resolves #98

---
+++ test/issue/color_scheme_test_98.php
@@ -0,0 +1,28 @@
+<?php // COLOR SCHEME TEST "Theme - Cobalt2/cobalt2.tmTheme" "PHP"
+
+class x
+{
+    /**
+     * @param array
+//              ^ fg=#0088ff fs=italic
+     * @param bool
+//              ^ fg=#0088ff fs=italic
+     * @param int
+//              ^ fg=#0088ff fs=italic
+     * @param public
+//              ^ fg=#0088ff fs=italic
+     * @param protected
+//              ^ fg=#0088ff fs=italic
+     * @param private
+//              ^ fg=#0088ff fs=italic
+     * @access private
+//              ^ fg=#0088ff fs=italic
+     * @access protected
+//              ^ fg=#0088ff fs=italic
+     * @access public
+//              ^ fg=#0088ff fs=italic
+     */
+    public function y()
+    {
+    }
+}
