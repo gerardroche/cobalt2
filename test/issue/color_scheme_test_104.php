dd21d0b94f4894be4865322a6d738ea778de29ed
Gerard Roche <gerardroche@users.noreply.github.com>
2016-08-19 17:08:08 +0100

    Fix wrong color for function call
    Closes #104

---
+++ test/issue/color_scheme_test_104.php
@@ -0,0 +1,9 @@
+<?php // COLOR TEST "Packages/Theme - Cobalt2/cobalt2.tmTheme" "PHP"
+
+if ( false === $i ) {
+    if ( ! $gmt )
+        $i = current_time( 'timestamp' );
+//              ^ fg=#ffee80
+    else
+        $i = time();
+}
